d6a4c90a47caa94354e67faf41262f52c5ed0f63
Michael Banack <bob5972@banack.net>
2014-01-05 17:40:43 -0800

    Stubbed out some simple types

--- main.c
+++ main.c
@@ -3,20 +3,56 @@
  */
 
 #include <stdio.h>
+#include "mbtypes.h"
+#include "random.h"
 
-int main()
+#define INVALID ((uint32)-1)
+
+typedef struct Mob {
+    uint32 id;
+    float x;
+    float y;
+} Mob;
+
+typedef struct World {
+    float width;
+    float height;
+} World;
+
+struct MainData {
+    Mob mobs[100];
+    World world;
+} mainData;
+
+int main(void)
 {
-    printf("Starting ants...\n");
-    //Setup
+    uint32 i;
+    bool done;
+    uint32 iterations;
 
-    //while (!done) {
-    //    // Run AI
-    //    // Run Physics
-    //    // Draw
-    //}
+    //Setup
+    printf("Starting ants...\n");
+    Util_Zero(&mainData, sizeof(mainData));
+    for (i = 0; i < ARRAYSIZE(mainData.mobs); i++) {
+        mainData.mobs[i].id = INVALID;
+    }
+
+    mainData.world.width = 100;
+    mainData.world.height = 100;
+
+    for (i = 0; i < ARRAYSIZE(mainData.mobs); i++) {
+        mainData.mobs[i].id = i;
+        mainData.mobs[i].x = Random_Float(0.0f, mainData.world.width);
+        mainData.mobs[i].y = Random_Float(0.0f, mainData.world.height);
+    }
+
+    for (iterations = 0; iterations < 100; iterations++) {
+        // Run AI
+        // Run Physics
+        // Draw
+    }
 
     //Cleanup
-
     printf("Done!\n");
     return 0;
 }
