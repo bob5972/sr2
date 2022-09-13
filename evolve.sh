643fae79318fea7d18e91394eea6f683fcd0fee3
Michael Banack <bob5972@banack.net>
2022-09-13 15:58:03 -0700

    added an extra kill screen

--- evolve.sh
+++ evolve.sh
@@ -20,6 +20,7 @@ SCREEN1_ITERATIONS=3
 SCREEN1_DEFECTIVE=0.1
 
 SCREENS_FILE="build/tmp/screenS.zoo";
+SCREENS_DEFECTIVE=0.1
 SCREENS_NEW_ITERATIONS=10
 SCREENS_STALE_ITERATIONS=1
 
@@ -29,60 +30,51 @@ OPTS="${OPTS} -S $SCENARIO"
 OPTS="${OPTS} -t $THREADS"
 
 ./compile.sh $BUILDTYPE
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: mutate
+echo 'sr2: mutate'
 build/sr2 mutate $OPTS --usePopulation $STABLE_FILE \
                        --outputFile $NOOB_FILE \
                        --mutationCount $NOOB_POP
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: measure screen1
+echo 'sr2: measure screen1'
 build/sr2 measure $OPTS --usePopulation $NOOB_FILE \
                         --controlPopulation $SCREEN1_FILE \
                         --loop $SCREEN1_ITERATIONS
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: kill screen1
+echo 'sr2: kill screen1'
 build/sr2 kill $OPTS --usePopulation $NOOB_FILE \
                      --defectiveLevel $SCREEN1_DEFECTIVE \
                      --resetAfter
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: measure screenS
+echo 'sr2: measure screenS (noob)'
 build/sr2 measure $OPTS --usePopulation $NOOB_FILE \
                         --controlPopulation $SCREENS_FILE \
                         --loop $SCREENS_NEW_ITERATIONS
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
+
+echo 'sr2: kill screenS (noob)'
+build/sr2 kill $OPTS --usePopulation $NOOB_FILE \
+                     --defectiveLevel $SCREENS_DEFECTIVE
+if [ $? != 0 ]; then exit $? ; fi;
 
 echo sr2: merge
 build/sr2 merge $OPTS --usePopulation $STABLE_FILE \
                       --inputPopulation $NOOB_FILE
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: measure screenS
+echo 'sr2: measure screenS (stable)'
 build/sr2 measure $OPTS --usePopulation $STABLE_FILE \
                         --controlPopulation $SCREENS_FILE \
                         --loop $SCREENS_STALE_ITERATIONS
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
 
-echo sr2: kill screenS
+echo 'sr2: kill screenS (stable)'
 build/sr2 kill $OPTS --usePopulation $STABLE_FILE \
+                     --defectiveLevel $SCREENS_DEFECTIVE \
+                     --minPop $STABLE_POP \
                      --maxPop $STABLE_POP
-if [ $? != 0 ]; then
-    exit $?
-fi;
+if [ $? != 0 ]; then exit $? ; fi;
