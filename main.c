a28254174fed3966e5932e67fe988773d7e03a92
Michael Banack <bob5972@banack.net>
2022-12-21 16:59:58 -0800

    switch main fleet to Bineural2

--- main.c
+++ main.c
@@ -220,7 +220,7 @@ static void MainLoadDefaultPlayers(void)
         mainData.players[p].aiType = FLEET_AI_NEURAL12;
         p++;
 
-        mainData.players[p].aiType = FLEET_AI_BINEURAL1;
+        mainData.players[p].aiType = FLEET_AI_BINEURAL2;
         p++;
 
         //mainData.players[p].playerName = "HoldMod";
